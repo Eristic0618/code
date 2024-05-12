601f530cc3649ebf0d03a3103fd7ea3f3df3ec01
Eristic <2779752984@qq.com>
2024-05-12 16:32:56 +0800

    21.合并两个有序链表(递归)

---
+++ 21.合并两个有序链表(递归).cpp
@@ -0,0 +1,23 @@
+// https://leetcode.cn/problems/merge-two-sorted-lists/description/
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        if (list1 == nullptr)
+            return list2;
+        if (list2 == nullptr)
+            return list1;
+        if (list1->val <= list2->val)
+        {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else
+        {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
