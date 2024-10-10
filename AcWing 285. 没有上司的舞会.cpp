aa274596c30035b35df0cff27ff24ce465e9b3cc
Eristic <2779752984@qq.com>
2024-10-10 09:17:16 +0800

    AcWing 285. 没有上司的舞会

---
+++ AcWing 285. 没有上司的舞会.cpp
@@ -0,0 +1,51 @@
+// https://www.acwing.com/problem/content/287/
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int N = 6010;
+
+int happy[N];
+int n;
+int e[N], ne[N], h[N], idx;
+int f[N][2];
+bool has_father[N];
+
+void add(int a, int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+void dfs(int u)
+{
+    f[u][1] = happy[u];
+    for (int i = h[u]; i != -1; i = ne[i])
+    {
+        int j = e[i];
+        dfs(j);
+        f[u][0] += max(f[j][0], f[j][1]);
+        f[u][1] += f[j][0];
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> happy[i];
+    memset(h, -1, sizeof h);
+    for (int i = 1; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        has_father[a] = true;
+        add(b, a);
+    }
+    int root = 1;
+    while (has_father[root])
+        root++;
+
+    dfs(root);
+    cout << max(f[root][0], f[root][1]) << endl;
+    return 0;
+}
